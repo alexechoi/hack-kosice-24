a3986ee39dc5dca4f71fcab862fbb71b046a73e2
alexchvod <uceiac8@ucl.ac.uk>
2024-04-06 18:47:09 +0200

    Enabled tabbar navigation

--- src/components/Tabbar/Tabbar.tsx
+++ src/components/Tabbar/Tabbar.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Tabbar.css';
 
 export const Tabbar = () => {
+
+    let navigate = useNavigate();
+
+    const navigateTo = (path) => {
+        navigate(path);
+    };
+
     return (
         <div className="tabbar">
-            <div className="tab">
+            <div className="tab" onClick={() => navigateTo('/')}>
                 <span className="icon">🏠</span>
                 <span className="label">Home</span>
             </div>
-            <div className="tab">
+            <div className="tab" onClick={() => navigateTo('/games')}>
                 <span className="icon">🎮</span>
                 <span className="label">Games</span>
             </div>
-            <div className="tab">
+            <div className="tab" onClick={() => navigateTo('/tips')}>
                 <span className="icon">💡</span>
                 <span className="label">Advice</span>
             </div>
