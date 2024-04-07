ab4e22f1c88056ee51372734ebffaf455df6dc91
Zdeněk Vychodil <vychodil.zdenek@outlook.com>
2024-04-07 08:50:05 +0200

    add tatra mini logo to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/tatra-dark-mini.png';
 
 export const Navbar = () => {
     let navigate = useNavigate();
@@ -14,6 +15,9 @@ export const Navbar = () => {
         <nav className="navbar">
             <div className="navbar-container">
                 <h1 className="navbar-title">StockSnake</h1>
+                <div className="tatra-dark-mini">
+                    <img src={logo} alt="Tatra dark mini"/>
+                </div>
                 <div className="navbar-icons">
                     <Link to="/search" className="icon-button">🔍</Link>
                 </div>
